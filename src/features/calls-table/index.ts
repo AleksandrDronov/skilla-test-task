5c3f00e7d78116b1374e3fc1c2bd363d9fe1f16c
Aleksandr Dronov <dronovspb@gmail.com>
2026-06-13 18:06:50 +0300

    refactor: remove unused CallsTableStatusProps type export
    - Eliminated the export of CallsTableStatusProps from the calls-table index file to streamline the codebase and remove redundancy.

--- src/features/calls-table/index.ts
+++ src/features/calls-table/index.ts
@@ -1,4 +1,3 @@
 export { CallsTable } from './ui/CallsTable/CallsTable'
 export { CallsTableStatus } from './ui/CallsTableStatus/CallsTableStatus'
-export type { CallsTableStatusProps } from './ui/CallsTableStatus/CallsTableStatus'
 export { useCallsData } from './lib/useCallsData'
